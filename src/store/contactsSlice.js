bbe03dfc8910c36d40884e8d082ea0b6ca5272e5
Savinov94 <134193164+Savinov94@users.noreply.github.com>
2023-12-26 21:06:50 +0100

    3refs

--- src/store/contactsSlice.js
+++ src/store/contactsSlice.js
@@ -1,10 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const contactsSlice = createSlice({
-  name: 'contacts',
+  name: 'contacts ',
   initialState: {
     contacts: [],
-    filter: '',
+    filter: "",
   },
 
   reducers: {
